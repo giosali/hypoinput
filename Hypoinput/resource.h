50354a7b0ae81d12345bffd4477e595e84db07f5
giosali <70548443+giosali@users.noreply.github.com>
2022-06-14 20:47:01 -0500

    Add context menu for notification icon

--- Hypoinput/resource.h
+++ Hypoinput/resource.h
@@ -2,14 +2,18 @@
 // Microsoft Visual C++ generated include file.
 // Used by Hypoinput.rc
 //
+#define IDR_NOTIFICATIONICONMENU        4
 #define IDI_NOTIFICATIONICON            101
+#define IDM_RUNATSTARTUP                40001
+#define IDM_ENABLE                      40002
+#define IDM_EXIT                        40003
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        102
-#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_RESOURCE_VALUE        103
+#define _APS_NEXT_COMMAND_VALUE         40008
 #define _APS_NEXT_CONTROL_VALUE         1001
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
