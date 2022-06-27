b9670e22a646bbda499f73ab3437480a78ec5ea9
giosali <70548443+giosali@users.noreply.github.com>
2022-06-26 22:53:56 -0500

    Add function for converting strings to wstrings

--- Hypoinput/utils.cpp
+++ Hypoinput/utils.cpp
@@ -10,6 +10,14 @@ std::string wstringToString(const std::wstring& ws)
     return buffer;
 }
 
+std::wstring stringToWString(const std::string& s)
+{
+    const int size = MultiByteToWideChar(CP_UTF8, 0, s.c_str(), -1, NULL, 0);
+    std::wstring ws(size, 0);
+    MultiByteToWideChar(CP_UTF8, 0, s.c_str(), -1, &ws.at(0), size);
+    return ws;
+}
+
 bool startsWith(const std::string& instance, const std::string& value)
 {
     if (!instance.empty() && value.empty()) {
--- Hypoinput/utils.h
+++ Hypoinput/utils.h
@@ -6,6 +6,7 @@
 namespace utils {
 
 std::string wstringToString(const std::wstring&);
+std::wstring stringToWString(const std::string&);
 bool startsWith(const std::string&, const std::string&);
 
 } // namespace utils
