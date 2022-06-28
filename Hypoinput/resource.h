daa33d06fa13ea9ce15c0760626aba636966b299
giosali <70548443+giosali@users.noreply.github.com>
2022-06-27 20:40:23 -0500

    Add IDM_OPENFILE resource to notification icon context menu

--- Hypoinput/resource.h
+++ Hypoinput/resource.h
@@ -6,14 +6,15 @@
 #define IDI_NOTIFICATIONICON            101
 #define IDM_RUNATSTARTUP                40001
 #define IDM_ENABLE                      40002
-#define IDM_EXIT                        40003
+#define IDM_OPENFILE                    40003
+#define IDM_EXIT                        40004
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        103
-#define _APS_NEXT_COMMAND_VALUE         40008
+#define _APS_NEXT_COMMAND_VALUE         40011
 #define _APS_NEXT_CONTROL_VALUE         1001
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
