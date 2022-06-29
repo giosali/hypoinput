29272368fa01d72788d5e388ac0b1628ac204045
giosali <70548443+giosali@users.noreply.github.com>
2022-06-28 21:15:34 -0500

    Remove functions

--- Hypoinput/utils.h
+++ Hypoinput/utils.h
@@ -10,11 +10,7 @@ namespace utils {
 std::string wstringToString(const std::wstring&);
 std::wstring stringToWString(const std::string&);
 bool startsWith(const std::string&, const std::string&);
-std::string replace(std::string, char);
-std::string replace(std::string, const std::string&);
-std::string trim(const std::string&, char = ' ');
 std::string ltrim(const std::string&, char = ' ');
 std::string rtrim(const std::string&, char = ' ');
-std::vector<std::string> split(const std::string&, char = ' ');
 
 } // namespace utils
