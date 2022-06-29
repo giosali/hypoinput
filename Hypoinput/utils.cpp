4d5cb626aaac5547771f7f63607086e76e7a90dc
giosali <70548443+giosali@users.noreply.github.com>
2022-06-28 21:10:17 -0500

    Add functions

--- Hypoinput/utils.cpp
+++ Hypoinput/utils.cpp
@@ -28,6 +28,50 @@ bool startsWith(const std::string& instance, const std::string& value)
     return instance.rfind(value, 0) == 0;
 }
 
+std::string replace(std::string s, char ch)
+{
+    s.erase(std::remove(s.begin(), s.end(), ch), s.end());
+    return s;
+}
+
+std::string replace(std::string oldValue, const std::string& newValue)
+{
+    size_t length = newValue.length();
+    while (true) {
+        size_t pos = oldValue.find(newValue);
+        if (pos == std::string::npos) {
+            break;
+        }
+
+        oldValue.erase(pos, length);
+    }
+
+    return oldValue;
+}
+
+std::string trim(const std::string& s, char ch)
+{
+    size_t start = 0;
+    size_t end = s.length() - 1;
+
+    // Trim head
+    for (; start < s.length(); start++) {
+        if (s[start] != ch) {
+            break;
+        }
+    }
+
+    // Trim tail
+    for (; end >= start; end--) {
+        if (s[end] != ch) {
+            break;
+        }
+    }
+
+    size_t length = end - start + 1;
+    return s.substr(start, length);
+}
+
 std::string ltrim(const std::string& s, char ch)
 {
     size_t i = 0;
@@ -50,6 +94,16 @@ std::string rtrim(const std::string& s, char ch)
     }
 
     return s.substr(0, i + 1);
+}
+
+std::vector<std::string> split(const std::string& text, char ch)
+{
+    std::vector<std::string> strings;
+    for (std::string line; std::getline(std::stringstream(text), line, ch);) {
+        strings.push_back(line);
+    }
+
+    return strings;
 }
 
 } // namespace utils
