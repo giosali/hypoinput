46dfbb08ebd820bf6d4069b775724ad71f95749a
giosali <70548443+giosali@users.noreply.github.com>
2022-06-30 00:16:19 -0500

    Add function for writing to files

--- Hypoinput/file.cpp
+++ Hypoinput/file.cpp
@@ -18,4 +18,21 @@ void create(const std::filesystem::path& path)
     ofs.close();
 }
 
+void write(const std::filesystem::path& path, const std::string& text)
+{
+    // Creates any missing parent directories.
+    std::filesystem::create_directories(path.parent_path());
+
+    std::ofstream ofs(path);
+    if (ofs.is_open()) {
+        char bom[] = { (char)0xEF, (char)0xBB, (char)0xBF, '\0' };
+        std::vector<std::string> lines = utils::split(text, '\n');
+        for (int i = 0; i < lines.size(); i++) {
+            ofs << utils::trim(utils::replace(lines[i], std::string(bom)), '\r') << std::endl;
+        }
+    }
+
+    ofs.close();
+}
+
 }
--- Hypoinput/file.h
+++ Hypoinput/file.h
@@ -1,10 +1,12 @@
 #pragma once
 
+#include "utils.h"
 #include <filesystem>
 #include <fstream>
 
 namespace file {
 
 void create(const std::filesystem::path&);
+void write(const std::filesystem::path&, const std::string&);
 
 } // namespace file
