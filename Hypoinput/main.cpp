a7b72996d795035248491f5c40c695a7bab05b25
giosali <70548443+giosali@users.noreply.github.com>
2022-07-09 21:39:34 -0500

    Rearrange switch labels

--- Hypoinput/main.cpp
+++ Hypoinput/main.cpp
@@ -89,6 +89,44 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
     static UINT s_uTaskbarRestart = 0;
 
     switch (msg) {
+    case WM_CREATE:
+        s_uTaskbarRestart = RegisterWindowMessage(TEXT("TaskbarCreated"));
+
+        if (!addNotificationIcon(hWnd)) {
+            return -1;
+        }
+
+        if (!g_settings.m_exists) {
+            // Writes the sample .INI resource file to application data
+            // if it doesn't exist.
+            std::string resourceText = environment::getResource(IDR_SETTINGSINI, L"INI");
+            file::write(g_settings.m_filePath, resourceText);
+            g_settings = ini::IniFile(g_settings.m_filePath);
+
+            // Causes application to run at startup.
+            registerRunValue();
+        }
+
+        expansions::TextExpansionManager::init();
+
+        g_keyboardHook = keyboard::KeyboardHook(onKeyDown);
+        g_keyboardHook.add(g_hInst);
+
+        // Sets up a file system watcher for the text expansions file.
+        g_textExpansionsFsw = filesystemwatcher::FileSystemWatcher(environment::getFolderPath(environment::SpecialFolder::HypoinputApplicationData), onTextExpansionsChanged);
+        g_textExpansionsFsw.watch(FILE_NOTIFY_CHANGE_LAST_WRITE);
+
+        // Creates PowerShell files.
+        file::write(environment::getFilePath(environment::SpecialFile::AddTextExpansion), environment::getResource(IDR_ADDTEXTEXPANSIONPS1, L"PS1"));
+        file::write(environment::getFilePath(environment::SpecialFile::EditTextExpansions), environment::getResource(IDR_EDITTEXTEXPANSIONSPS1, L"PS1"));
+        file::write(environment::getFilePath(environment::SpecialFile::Common), environment::getResource(IDR_COMMONPS1, L"PS1"));
+        break;
+    case WM_DESTROY:
+        deleteNotificationIcon(hWnd);
+        g_textExpansionsFsw.stop();
+        PostQuitMessage(0);
+        g_keyboardHook.remove();
+        break;
     case WM_COMMAND:
         switch (LOWORD(wParam)) {
         case IDM_ENABLE:
@@ -131,44 +169,6 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
             return DefWindowProc(hWnd, msg, wParam, lParam);
         }
 
-        break;
-    case WM_CREATE:
-        s_uTaskbarRestart = RegisterWindowMessage(TEXT("TaskbarCreated"));
-
-        if (!addNotificationIcon(hWnd)) {
-            return -1;
-        }
-
-        if (!g_settings.m_exists) {
-            // Writes the sample .INI resource file to application data
-            // if it doesn't exist.
-            std::string resourceText = environment::getResource(IDR_SETTINGSINI, L"INI");
-            file::write(g_settings.m_filePath, resourceText);
-            g_settings = ini::IniFile(g_settings.m_filePath);
-
-            // Causes application to run at startup.
-            registerRunValue();
-        }
-
-        expansions::TextExpansionManager::init();
-
-        g_keyboardHook = keyboard::KeyboardHook(onKeyDown);
-        g_keyboardHook.add(g_hInst);
-
-        // Sets up a file system watcher for the text expansions file.
-        g_textExpansionsFsw = filesystemwatcher::FileSystemWatcher(environment::getFolderPath(environment::SpecialFolder::HypoinputApplicationData), onTextExpansionsChanged);
-        g_textExpansionsFsw.watch(FILE_NOTIFY_CHANGE_LAST_WRITE);
-
-        // Creates PowerShell files.
-        file::write(environment::getFilePath(environment::SpecialFile::AddTextExpansion), environment::getResource(IDR_ADDTEXTEXPANSIONPS1, L"PS1"));
-        file::write(environment::getFilePath(environment::SpecialFile::EditTextExpansions), environment::getResource(IDR_EDITTEXTEXPANSIONSPS1, L"PS1"));
-        file::write(environment::getFilePath(environment::SpecialFile::Common), environment::getResource(IDR_COMMONPS1, L"PS1"));
-        break;
-    case WM_DESTROY:
-        deleteNotificationIcon(hWnd);
-        g_textExpansionsFsw.stop();
-        PostQuitMessage(0);
-        g_keyboardHook.remove();
         break;
     case WMAPP_NOTIFYCALLBACK:
         switch (LOWORD(lParam)) {
