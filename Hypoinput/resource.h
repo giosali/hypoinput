68e6e0f91e3d24fba2e077882f0f26284a3cf4b1
giosali <70548443+giosali@users.noreply.github.com>
2022-07-13 21:17:34 -0500

    Add notification icon context menu item to check for updates

--- Hypoinput/resource.h
+++ Hypoinput/resource.h
@@ -13,14 +13,15 @@
 #define IDM_OPENFILE                    40003
 #define IDM_ADDTEXTEXPANSION            40004
 #define IDM_EDITTEXTEXPANSIONS          40005
-#define IDM_EXIT                        40006
+#define IDM_CHECKFORUPDATE              40006
+#define IDM_EXIT                        40007
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        108
-#define _APS_NEXT_COMMAND_VALUE         40018
+#define _APS_NEXT_COMMAND_VALUE         40020
 #define _APS_NEXT_CONTROL_VALUE         1001
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
