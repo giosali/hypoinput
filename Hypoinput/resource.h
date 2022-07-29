17aacc229214c8ceeec68867d5415ebff36d89d2
giosali <70548443+giosali@users.noreply.github.com>
2022-07-28 23:39:48 -0500

    Add menu items for language options

--- Hypoinput/resource.h
+++ Hypoinput/resource.h
@@ -10,18 +10,21 @@
 #define IDI_NOTIFICATIONICON            107
 #define IDM_ENABLE                      40001
 #define IDM_RUNATSTARTUP                40002
-#define IDM_OPENFILE                    40003
-#define IDM_ADDTEXTEXPANSION            40004
-#define IDM_EDITTEXTEXPANSIONS          40005
-#define IDM_CHECKFORUPDATES             40006
-#define IDM_EXIT                        40007
+#define IDM_LANGUAGE_ENGLISH			40003
+#define IDM_LANGUAGE_ESPANOL			40004
+#define IDM_LANGUAGE_FRANCAIS			40005
+#define IDM_OPENFILE                    40006
+#define IDM_ADDTEXTEXPANSION            40007
+#define IDM_EDITTEXTEXPANSIONS          40008
+#define IDM_CHECKFORUPDATES             40009
+#define IDM_EXIT                        40010
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        108
-#define _APS_NEXT_COMMAND_VALUE         40021
+#define _APS_NEXT_COMMAND_VALUE         40028
 #define _APS_NEXT_CONTROL_VALUE         1001
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
